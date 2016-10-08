9774b46fb36e0345e9686a814804bdea8efecd62
Nick Matvyeyev <mnasoft@gmail.com>
2016-10-08 20:57:20 +0300

    новый файл:    mnas-vse-gost-audio.lisp

---
+++ mnas-vse-gost-audio.lisp
@@ -0,0 +1,10 @@
+;;;; mnas-vse-gost-audio.lisp
+
+(in-package #:mnas-vse-gost)
+;;; "mnas-vse-gost" goes here. Hacks and glory await!
+
+(defparameter *audio*
+  '("/audio/Vicente Amigo Canción de Laura- DESTINO.mp3"
+    "/audio/dzhejms_last_-_odinokij_pastuh_(zvukoff.ru).mp3"))
+
+
