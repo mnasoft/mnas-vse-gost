86170633dfdda1d313c6ff0a36aac99b9d9cbad0
Mykola Matvyeyev <mnasoft@gmail.com>
2023-03-12 20:05:17 +0200

    Sun 12 Mar 2023 06:05:17 PM UTC

--- package.lisp
+++ package.lisp
@@ -4,6 +4,4 @@
   (:use #:cl #:postmodern #:hunchentoot #:cl-who #:mnas-string #:mnas-site #:mnas-passwd #:mnas-path #:mnas-site-template)
   (:export gost-start gost-stop))
 
-;;;;(declaim (optimize (space 0) (compilation-speed 0)  (speed 0) (safety 3) (debug 3)))
-
-;;;; (declaim (optimize (compilation-speed 0) (debug 3) (safety 0) (space 0) (speed 0)))
+(in-package :mnas-vse-gost)
